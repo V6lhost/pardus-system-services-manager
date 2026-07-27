5ffa93b76c0d0e26e6b1be92a9157c1e6654dbb3
V6lhost <v6lhost@proton.me>
2026-07-27 13:58:35 +0300

    MainWindow: add UnitDetails and ApplicationDetails boxes and fill it with information and connect buttons

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -3,12 +3,22 @@ from pathlib import Path
 from xdg.DesktopEntry import DesktopEntry
 import sys
 import shutil
+import subprocess
+import json
 
+ # Using subprocess is way faster than listing units with dbus
+ 
 def list_systemd_units():
-    bus = SystemBus()
-    systemd = bus.get(".systemd1", "/org/freedesktop/systemd1")
-    units = systemd.ListUnitFiles()
-    return units
+    command = ["systemctl", "list-unit-files", "-o", "json"]
+    
+    output = subprocess.run(
+        command,
+        capture_output=True,
+        text=True,
+        check=True,
+        timeout=5
+    )
+    return json.loads(output.stdout)
 
 def get_unit_active_status(unit_name):
     bus = SystemBus()
@@ -19,6 +29,107 @@ def get_unit_active_status(unit_name):
         return unit.ActiveState
     except Exception:
         return "activation is not possible"
+
+def get_unit_description(unit_name):
+    try:
+        bus = SystemBus()
+        systemd = bus.get(".systemd1", "/org/freedesktop/systemd1")
+        unit_path = systemd.GetUnit(unit_name)
+        unit_obj = bus.get(".systemd1", unit_path)
+        return unit_obj.Description
+        
+    except Exception as e:
+        return "No description"
+
+def get_unit_exit_status(unit_name):
+    try:
+        bus = SystemBus()
+        systemd = bus.get(".systemd1", "/org/freedesktop/systemd1")
+        unit_path = systemd.GetUnit(unit_name)
+        props = bus.get(".systemd1", unit_path)
+        
+        code = getattr(props, "ExecMainCode", None)
+        status = getattr(props, "ExecMainStatus", None)
+        result = getattr(props, "Result", None)
+        
+        return {
+            "code": code,
+            "status": status,
+            "result": result
+        }
+
+    except:
+        return {
+            "code": "Unavailable",
+            "status": "Unavailable",
+            "result": "Unavailable"
+        }
+
+def get_unit_logs(unit_name):
+    try:
+        command = ["journalctl", "-u", unit_name, "--no-pager"]
+        
+        output = subprocess.run(
+            command,
+            capture_output=True,
+            text=True,
+            check=True,
+            timeout=5
+        )
+        
+        return output.stdout
+        
+    except subprocess.CalledProcessError as e:
+        return "Error while getting logs"
+
+def get_unit_file_path(unit_name):
+        bus = SystemBus()
+        systemd = bus.get(".systemd1", "/org/freedesktop/systemd1")
+        unit_path = systemd.LoadUnit(unit_name)
+        props = bus.get(".systemd1", unit_path)
+        return getattr(props, "FragmentPath", "")
+
+def run_systemctl_command(action, unit_name):
+    try:
+        cmd = ["pkexec", "systemctl", action, unit_name]
+        
+        result = subprocess.run(
+            cmd,
+            capture_output=True,
+            text=True,
+            check=False
+        )
+        
+        if result.returncode == 0:
+            return True
+        else:
+            return False
+            
+    except Exception:
+        return False
+
+def unit_enable(unit_name):
+    return run_systemctl_command("enable", unit_name)
+
+def unit_disable(unit_name):
+    return run_systemctl_command("disable", unit_name)
+
+def unit_start(unit_name):
+    return run_systemctl_command("start", unit_name)
+
+def unit_stop(unit_name):
+    return run_systemctl_command("stop", unit_name)
+
+def unit_restart(unit_name):
+    return run_systemctl_command("restart", unit_name)
+
+def save_logs_to_temporary_file(unit_name):
+    logs = get_unit_logs(unit_name)
+    logfile = Path("/tmp") / f"log-{unit_name}.txt"
+    with open(logfile, "w", encoding="UTF_8") as f:
+        f.write(logs)
+    
+    return logfile
 
 def list_desktop_entries(path):
     files = list(path.iterdir())
@@ -49,3 +160,6 @@ def copy_desktop_entry(source, destination):
         return
     
     shutil.copy(source, destination)
+
+def remove_autostart_entry_file(entry):
+    entry.unlink(missing_ok=True)
--- src/main.py
+++ src/main.py
@@ -5,20 +5,24 @@ from PySide6.QtWidgets import (
     QMainWindow,
     QDialog,
     QDialogButtonBox,
-    QTableWidget,
-    QTableWidgetItem,
     QHeaderView
 )
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtCore import (
     QFile,
     Qt,
-    QSortFilterProxyModel
+    QSortFilterProxyModel,
+    QUrl,
+    QSize,
+    QThread,
+    QTimer,
+    Signal
 )
 from PySide6.QtGui import (
     QStandardItem,
     QStandardItemModel,
-    QIcon
+    QIcon,
+    QDesktopServices
 )
 
 from helper_functions import *
@@ -34,11 +38,25 @@ class UiLoader(QUiLoader):
             return self.baseinstance
         return super().createWidget(class_name, parent, name)
 
+class UnitListReloadThread(QThread):
+    # Use qthread to prevent gui lags. otherwise reloading unit list blockes the gui thread and causes freeze(and sometimes crashes)
+    add_units = Signal(list)
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+    
+    def run(self):
+        units = list_systemd_units()
+        for row,unit in enumerate(units):
+            units[row]["active_status"] = get_unit_active_status(unit["unit_file"])
+        
+        units = sorted(units, key=lambda x: x["unit_file"].lower())
+        self.add_units.emit(units)
+
 def load_ui(ui_path, baseinstance=None):
     loader = UiLoader(baseinstance)
     ui_file = QFile(ui_path)
     if not ui_file.open(QFile.ReadOnly):
-        print(f"Error while loading ui file: {ui_path}")
         return None
     widget = loader.load(ui_file)
     ui_file.close()
@@ -59,6 +77,9 @@ class MainWindow(QMainWindow):
         self.tableModel = QStandardItemModel(self)
         self.tableViewUnits.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
 
+        self.groupBoxUnitDetails.hide()
+        self.groupBoxApplicationDetails.hide()
+
         self.tableProxyModel = QSortFilterProxyModel(self)
         self.tableProxyModel.setSourceModel(self.tableModel)
         self.tableProxyModel.setFilterKeyColumn(0)
@@ -67,22 +88,44 @@ class MainWindow(QMainWindow):
 
         self.stackedWidget.setCurrentIndex(0)
 
+        self.units = []
+        
+        self.thread = UnitListReloadThread(self)
+        self.thread.add_units.connect(self.add_units_to_gui)
+
+        self.timerUpdateUnits = QTimer()
+        self.timerUpdateUnits.timeout.connect(self.thread.start)
+        self.timerUpdateUnits.start(5000)
+
+        # call the thread manually once to load ui faster
+        self.thread.start()
+
+        self.add_autostart_applications_to_gui()
         self.connect_ui_events()
 
-        self.add_units_to_gui()
-        self.add_autostart_applications_to_gui()
+    def add_units_to_gui(self, units):
+        if units == self.units:
+            return
+        self.units = units
 
-    def add_units_to_gui(self):
-        units = list_systemd_units()
+        # get selected unit name to reselect it after updating table
+        self.selected_unit_name = None
+        selected_indexes = self.tableViewUnits.selectionModel().selectedRows()
+        if selected_indexes:
+            proxy_index = selected_indexes[0]
+            source_index = self.tableProxyModel.mapToSource(proxy_index)
+            name_item = self.tableModel.item(source_index.row(), 0)
+            if name_item:
+                self.selected_unit_name = name_item.text()
 
+        self.tableViewUnits.clearSelection()
         self.tableModel.clear()
         self.tableModel.setHorizontalHeaderLabels(["Name", "State", "Active Status"])
 
         for unit in units:
-            unit_path = unit[0]
-            unit_name = unit_path.split("/")[-1]
-            unit_state = unit[1]
-            unit_active_status = get_unit_active_status(unit_name)
+            unit_name = unit["unit_file"]
+            unit_state = unit["state"]
+            unit_active_status = unit["active_status"]
 
             item_name = QStandardItem(unit_name)
             item_state = QStandardItem(unit_state)
@@ -90,14 +133,62 @@ class MainWindow(QMainWindow):
 
             self.tableModel.appendRow([item_name, item_state, item_active_status])
 
-            if unit_active_status.lower() == "failed":
+            if unit_active_status == "failed":
                 item_active_status.setForeground(Qt.red)
-            elif unit_active_status.lower() == "active":
+            elif unit_active_status == "active":
                 item_active_status.setForeground(Qt.green)
-            elif unit_active_status.lower() == "inactive":
+            elif unit_active_status == "inactive":
                 item_active_status.setForeground(Qt.yellow)
             else:
                 item_active_status.setForeground(Qt.gray)
+        
+            # reselect the latest selected unit again. this also helps to update the unit description part automatically
+            if self.selected_unit_name:
+                for row in range(self.tableModel.rowCount()):
+                    item = self.tableModel.item(row, 0)
+                    if item and item.text() == self.selected_unit_name:
+                        source_index = self.tableModel.index(row, 0)
+                        proxy_index = self.tableProxyModel.mapFromSource(source_index)
+                        
+                        if proxy_index.isValid():
+                            self.tableViewUnits.setCurrentIndex(proxy_index)
+                            self.tableViewUnits.selectRow(proxy_index.row())
+
+    def fill_unit_details(self, unit_name, unit_state, unit_active_state, unit_active_state_color): # take unit state and active state from table instead of check again because checking again takes some time
+        self.groupBoxUnitDetails.show()
+        self.labelUnitNameText.setText(unit_name)
+
+        unit_description = get_unit_description(unit_name)
+        self.labelUnitDescriptionText.setText(unit_description)
+
+        self.radioButtonUnitEnabled.setChecked(True if unit_state == "enabled" else False)
+        self.radioButtonUnitEnabled.setEnabled(True if unit_state in ["enabled", "disabled", "alias"] else False)
+
+        self.pushButtonUnitStartRestart.setEnabled(True if unit_active_state != "activation is not possible" else False)
+        self.pushButtonUnitStop.setEnabled(True if unit_active_state == "active" else False)
+
+        self.labelUnitLoadStatusText.setText(unit_state)
+        self.labelUnitActiveStatusText.setText(unit_active_state)
+        self.labelUnitActiveStatusText.setStyleSheet(f"color: {unit_active_state_color};")
+    
+        unit_status = get_unit_exit_status(unit_name)
+        self.labelUnitSubStatusText.setText(f"{unit_status['status']}\n{unit_status['result']}")
+        self.labelUnitExitCodeText.setText(f"{unit_status['code']}")
+
+        self.pushButtonEditUnitFile.setEnabled(True if not "@" in unit_name else False)
+
+    def fill_application_details(self, entry):
+        self.groupBoxApplicationDetails.show()
+        application_details = get_application_details(entry)
+
+        theme_icon = QIcon.fromTheme(application_details["icon"])
+        pixmap = theme_icon.pixmap(QSize(64, 64))
+
+        self.labelIcon.setPixmap(pixmap)
+
+        self.labelApplicationNameText.setText(application_details["name"])
+        self.labelApplicationDescriptionText.setText(application_details["comment"])
+        self.labelEntryFilePath.setText(str(entry))
 
     def add_autostart_applications_to_gui(self):
         autostart_applications_dir = Path.home() / Path(".config/autostart")
@@ -108,6 +199,7 @@ class MainWindow(QMainWindow):
             item = QStandardItem(application_details["name"])
             icon = QIcon.fromTheme(application_details["icon"])
             item.setIcon(icon)
+            item.setData(entry, Qt.UserRole)
             self.listModel.appendRow(item)
     
     def add_application_to_autostart(self, entry_path):
@@ -117,11 +209,77 @@ class MainWindow(QMainWindow):
     
     def filter_units(self, text):
         self.tableProxyModel.setFilterFixedString(text)
+    
+    def open_file(self, path):
+        url = QUrl.fromLocalFile(path)
+        QDesktopServices.openUrl(url)
         
     def connect_ui_events(self):
         self.pushButtonAutostartApplications.toggled.connect(lambda page_id: self.stackedWidget.setCurrentIndex(page_id))
         self.pushButtonAddApplication.clicked.connect(self.open_applications_dialog)
         self.lineEditSearchUnits.textChanged.connect(self.filter_units)
+        self.tableViewUnits.selectionModel().selectionChanged.connect(self.on_unit_item_clicked)
+        self.pushButtonEditUnitFile.clicked.connect(self.open_edit_unit_warning_dialog)
+        self.pushButtonUnitShowLogs.clicked.connect(lambda: self.open_file(save_logs_to_temporary_file(self.labelUnitNameText.text())))
+    
+        self.listViewAutostartApplications.selectionModel().selectionChanged.connect(self.on_application_item_clicked)
+        self.pushButtonEditEntryFile.clicked.connect(lambda: self.open_file(Path(self.labelEntryFilePath.text())))
+        self.pushButtonRemoveFromAutostart.clicked.connect(self.remove_current_entry_file)
+
+        self.radioButtonUnitEnabled.clicked.connect(self.set_unit_enabled)
+        self.pushButtonUnitStartRestart.clicked.connect(self.start_restart_unit)
+        self.pushButtonUnitStop.clicked.connect(self.stop_unit)
+
+    def remove_current_entry_file(self):
+        entry = self.labelEntryFilePath.text()
+        path = Path(entry)
+        remove_autostart_entry_file(path)
+        self.add_autostart_applications_to_gui()
+
+    def on_unit_item_clicked(self, selected):
+        indexes = selected.indexes()
+        if not indexes:
+            return
+        
+        proxy_index = indexes[0]
+        source_index = self.tableProxyModel.mapToSource(proxy_index)
+        row = source_index.row()
+        name_item = self.tableModel.item(row, 0)
+        state_item = self.tableModel.item(row, 1)
+        active_state_item = self.tableModel.item(row, 2)
+        active_state_color = active_state_item.foreground().color()
+
+        if name_item:
+            self.fill_unit_details(name_item.text(), state_item.text(), active_state_item.text(), active_state_color.name())
+    
+    def on_application_item_clicked(self, selected):
+        indexes = selected.indexes()
+        if not indexes:
+            return
+
+        index = indexes[0]
+        self.fill_application_details(index.data(Qt.UserRole))
+
+    def set_unit_enabled(self):
+        self.radioButtonUnitEnabled.setEnabled(False)
+        if self.radioButtonUnitEnabled.isChecked():
+            unit_enable(self.labelUnitNameText.text())
+        else:
+            unit_disable(self.labelUnitNameText.text())
+        self.radioButtonUnitEnabled.setEnabled(True)
+    
+    def start_restart_unit(self):
+        self.pushButtonUnitStartRestart.setEnabled(False)
+        if self.labelUnitActiveStatusText.text() == "active":
+            unit_restart(self.labelUnitNameText.text())
+        else:
+            unit_start(self.labelUnitNameText.text())
+        self.pushButtonUnitStartRestart.setEnabled(True)
+
+    def stop_unit(self):
+        self.pushButtonUnitStop.setEnabled(False)
+        unit_stop(self.labelUnitNameText.text())
+        self.pushButtonUnitStop.setEnabled(True)
 
     def open_applications_dialog(self):
         applications_dialog = ApplicationsDialog(self)
@@ -129,6 +287,15 @@ class MainWindow(QMainWindow):
 
         if result == QDialog.Accepted:
             self.add_application_to_autostart(applications_dialog.entry_path)
+            self.add_autostart_applications_to_gui()
+    
+    def open_edit_unit_warning_dialog(self):
+        unit_name = self.labelUnitNameText.text()
+        warning_dialog = EditUnitWarningDialog(self, unit_name=unit_name)
+        result = warning_dialog.exec()
+
+        if result == QDialog.Accepted:
+            self.open_file(get_unit_file_path(unit_name))
 
 class ApplicationsDialog(QDialog):
     def __init__(self, parent=None):
@@ -187,6 +354,15 @@ class ApplicationsDialog(QDialog):
 
         self.accept()
 
+class EditUnitWarningDialog(QDialog):
+    def __init__(self, parent=None, unit_name=None):
+        super(EditUnitWarningDialog, self).__init__()
+
+        current_dir = Path(__file__).resolve().parent
+        ui_path = current_dir.parent / "ui" / "EditUnitWarningDialog.ui"
+        load_ui(ui_path, self)
+
+        self.labelUnitNameText.setText(unit_name)
 
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal.SIG_DFL) # Handle CTRL+C interrupt
